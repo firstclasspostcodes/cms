cda180b2e2991b8a7ba45ebbbe15ab2054449026
davidkelley <david.james.kelley@gmail.com>
2020-03-21 21:24:40 +0000

    refactor: 💡 removing obsolete comments from illustration

--- src/lib/components/Illustration/src/index.jsx
+++ src/lib/components/Illustration/src/index.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import Img from 'gatsby-image';
-// import { getFluidGatsbyImage } from 'gatsby-source-sanity';
 import { Pane } from '@firstclasspostcodes/sw14';
 
 import usePresentation from '../../../hooks/usePresentation';
@@ -16,37 +14,6 @@ export const Component = ({ image, ...props }) => {
   const [, projectId, dataset] = path.split('/');
 
   const maxWidth = Math.min(metadata.dimensions.width, 1024);
-
-  // const fluidProps = getFluidGatsbyImage(id, { maxWidth }, { projectId, dataset });
-
-  // const renderProps = {
-  //   alt: caption,
-  //   title: caption,
-  //   style: {
-  //     margin: 'auto',
-  //     maxWidth,
-  //   },
-  // };
-
-  // const imageTypeProps = {
-  //   id,
-  //   maxWidth,
-  //   projectId,
-  //   dataset,
-  // };
-
-  // if (typeof fluid === 'function') {
-  //   renderProps.fluid = fluid(imageTypeProps);
-
-  //   if (extension === 'svg') {
-  //     delete renderProps.fluid.srcWebp;
-  //     delete renderProps.fluid.srcSetWebp;
-  //   }
-  // }
-
-  // if (typeof fixed === 'function') {
-  //   renderProps.fixed = fixed(imageTypeProps);
-  // }
 
   return (
     <Pane {...props} {...presentationProps}>
@@ -67,7 +34,6 @@ export const Component = ({ image, ...props }) => {
           },
         },
       })}
-      {/* <Img alt={caption} title={caption} fluid={fluidProps} style={{ margin: 'auto', maxWidth }} /> */}
     </Pane>
   );
 };
