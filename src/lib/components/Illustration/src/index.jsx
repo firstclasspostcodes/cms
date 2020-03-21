2b6264bd57895f8795c0ecf9d6d50637bd3c745f
davidkelley <david.james.kelley@gmail.com>
2020-03-21 14:25:32 +0000

    feat: 🎸 enabling configurable <Illustration/> component

--- src/lib/components/Illustration/src/index.jsx
+++ src/lib/components/Illustration/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Img from 'gatsby-image';
-import { getFluidGatsbyImage } from 'gatsby-source-sanity';
+// import Img from 'gatsby-image';
+// import { getFluidGatsbyImage } from 'gatsby-source-sanity';
 import { Pane } from '@firstclasspostcodes/sw14';
 
 import usePresentation from '../../../hooks/usePresentation';
@@ -17,32 +17,74 @@ export const Component = ({ image, ...props }) => {
 
   const maxWidth = Math.min(metadata.dimensions.width, 1024);
 
-  const fluidProps = getFluidGatsbyImage(id, { maxWidth }, { projectId, dataset });
+  // const fluidProps = getFluidGatsbyImage(id, { maxWidth }, { projectId, dataset });
 
-  if (extension === 'svg') {
-    delete fluidProps.srcWebp;
-    delete fluidProps.srcSetWebp;
-  }
+  // const renderProps = {
+  //   alt: caption,
+  //   title: caption,
+  //   style: {
+  //     margin: 'auto',
+  //     maxWidth,
+  //   },
+  // };
+
+  // const imageTypeProps = {
+  //   id,
+  //   maxWidth,
+  //   projectId,
+  //   dataset,
+  // };
+
+  // if (typeof fluid === 'function') {
+  //   renderProps.fluid = fluid(imageTypeProps);
+
+  //   if (extension === 'svg') {
+  //     delete renderProps.fluid.srcWebp;
+  //     delete renderProps.fluid.srcSetWebp;
+  //   }
+  // }
+
+  // if (typeof fixed === 'function') {
+  //   renderProps.fixed = fixed(imageTypeProps);
+  // }
 
   return (
     <Pane {...props} {...presentationProps}>
-      <Img alt={caption} title={caption} fluid={fluidProps} style={{ margin: 'auto', maxWidth }} />
+      {Component.staticConfig.render({
+        alt: caption,
+        title: caption,
+        style: {
+          margin: 'auto',
+          maxWidth,
+        },
+        metadata: {
+          id,
+          maxWidth,
+          extension,
+          cms: {
+            projectId,
+            dataset,
+          },
+        },
+      })}
+      {/* <Img alt={caption} title={caption} fluid={fluidProps} style={{ margin: 'auto', maxWidth }} /> */}
     </Pane>
   );
 };
 
 Component.displayName = 'CMS.Illustration';
 
+Component.staticConfig = {
+  render: () => {
+    throw new Error(`Configure "illustration.render" for the <Illustration/> component.`);
+  },
+};
+
 Component.isType = type => type === 'illustration';
 
 Component.propTypes = {
   image: PropTypes.shape({
     caption: PropTypes.string.isRequired,
-    asset: PropTypes.shape({
-      _id: PropTypes.string.isRequired,
-      extension: PropTypes.string.isRequired,
-      path: PropTypes.string.isRequired,
-      metadata: PropTypes.object.isRequired,
-    }).isRequired,
+    asset: PropTypes.object.isRequired,
   }).isRequired,
 };
--- src/lib/config.js
+++ src/lib/config.js
@@ -1,8 +1,10 @@
 import { Interactable } from './components/Interactable';
+import { Illustration } from './components/Illustration';
 import { Page } from './components/Page';
 
 const configMap = [
   ['interactable', Interactable],
+  ['illustration', Illustration],
   ['page', Page],
 ];
 
